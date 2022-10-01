b6004f4318b7b6ad52b856229c98d6098cb62f47
Jaydon <pearcejaydon@gmail.com>
2022-10-02 00:34:01 +1000

    Update Score style

--- src/components/Scores.js
+++ src/components/Scores.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const Scores = ({ score, highScore, total }) => {
   return (
-    <div className="scores">
+    <header className="scores">
       <p>Current Score: {score}</p>
       <p>High Score: {highScore}</p>
       <p>Highest possible score: {total}</p>
-    </div>
+    </header>
   );
 };
 
