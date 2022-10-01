488d63eca5aa6b66829a9b5218abb44e653bda49
Jaydon <pearcejaydon@gmail.com>
2022-10-01 22:45:31 +1000

    Add card grid and basic style to cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,6 +4,9 @@ const Card = ({ src, name }) => {
   return (
     <div className="card">
       <img src={src} alt={name + " from the Overwatch videogame"} />
+      <div className="info">
+        <p>{name}</p>
+      </div>
     </div>
   );
 };
