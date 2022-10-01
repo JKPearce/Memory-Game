8b94c76142c268d3d9402f32cd9ac93204ea9fbc
Jaydon <pearcejaydon@gmail.com>
2022-10-02 00:01:13 +1000

    Add shuffle

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Card from "./Card";
 import Scores from "./Scores";
 import IMAGES from "../images/images";
@@ -10,6 +10,7 @@ const Game = () => {
   const [score, setScore] = useState(0);
   const [highScore, setHighScore] = useState(0);
   const [clickedArray, addClickedItem] = useState([]);
+  const [images, setImages] = useState(IMAGES);
 
   function checkWin(e) {
     const target = e.currentTarget.id;
@@ -23,16 +24,40 @@ const Game = () => {
 
     addClickedItem((prevState) => [...prevState, target]);
     setScore(score + 1);
+    shuffle();
   }
 
   function reset() {
     setScore(0);
     addClickedItem([]);
+    shuffle();
   }
+
+  function shuffle() {
+    let m = images.length,
+      t,
+      i,
+      tempArray = images;
+    // While there remain elements to shuffle…
+    while (m) {
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+
+      // And swap it with the current element.
+      t = tempArray[m];
+      tempArray[m] = tempArray[i];
+      tempArray[i] = t;
+    }
+    setImages(tempArray);
+  }
+
+  useEffect(() => {
+    shuffle();
+  }, []);
 
   return (
     <main className="main">
-      <Scores score={score} highScore={highScore} total={IMAGES.length} />
+      <Scores score={score} highScore={highScore} total={images.length} />
       <div className="card-wrapper">
         {IMAGES.map((character) => (
           <Card
